15a9dd5cc5d862a3c90a2858c795001ae66aed34
Melissa Kyd <mel_kyd@hotmail.com>
2018-01-27 14:55:06 +1030

    Selection sorting algorithm in Java

---
+++ Java/SortingAlgorithm/src/SortingAlg.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class SortingAlg {
+
+	public static void main(String[] args) {
+		int[] lst = {4,9,7,1,3,6,5};
+//		int[] lst = {24,19,77,1,305,46,55}; //test different nums
+
+		    int n = lst.length;
+		    int temp;
+		    for (int i = 0; i < n; i++) {
+		        int smallest = lst[i];
+		        for (int j = i+1; j < n; j++) {
+		            if (lst[j] < smallest) {
+		            	smallest = lst[j];
+		                temp = lst[i];
+		                lst[i] = smallest;
+		                lst[j] = temp;
+		            }
+		        } 
+		    }
+	    System.out.println(Arrays.toString(lst));
+
+	}
+
+}
